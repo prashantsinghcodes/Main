a714c5ff8e22b153416724d749524097fe547ac8
Prashant Singh <prashantsingh4787@gmail.com>
2023-01-15 22:54:19 +0530

    Array - simple programs

--- src/dsa/array/easy/promblemsonarray/P2.java
+++ src/dsa/array/easy/promblemsonarray/P2.java
@@ -2,6 +2,7 @@ package dsa.array.easy.promblemsonarray;
 
 import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Map;
 
 /*
  Check if two arrays are equal using sorting
@@ -11,7 +12,7 @@ public class P2 {
     public static void main(String[] args) {
         int[] arr1 = {1, 2, 5, 4, 0};
         int[] arr2 = {2, 4, 5, 0, 1};
-        if(areEqualBySorting(arr1, arr2))
+        if(areEqualByHashing(arr1, arr2))
             System.out.print("Yes");
         else
             System.out.print("No");
@@ -31,6 +32,31 @@ public class P2 {
         for(int i = 0; i < n; i++) {
             if(arr1[i] != arr2[i])
                 return false;
+        }
+        return true;
+    }
+
+    private static boolean areEqualByHashing(int[] arr1, int[] arr2) {
+        int n = arr1.length, m = arr2.length;
+        Map<Integer, Integer> hashMap = new HashMap<>();
+        for(int i = 0; i < n; i++) {
+            if(!hashMap.containsKey(arr1[i]))
+                hashMap.put(arr1[i], 1);
+            else {
+                hashMap.put(arr1[i], hashMap.get(arr1[i]) + 1);
+            }
+        }
+
+        for(int i = 0; i < m; i++) {
+            if(!hashMap.containsKey(arr2[i]))
+                return false;
+
+            if(hashMap.get(arr2[i]) == 0)
+                return false;
+
+            int counter = hashMap.get(arr2[i]);
+            --counter;
+            hashMap.put(arr2[i], counter);
         }
         return true;
     }
